ac67d0aa23ad46e9de0d1a45a2fb61ad644ad665
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2022-05-08 13:00:54 +0800

    移動圖示更新

--- Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/SystemCtrl.cs
+++ Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/SystemCtrl.cs
@@ -14,8 +14,18 @@ namespace coolRain
 		[SerializeField, Header("虛擬搖桿")]
 		private Joystick joystick;
 
-		[SerializeField, Header("移動速度")]
+		[SerializeField, Header("移動速度") , Range(0, 300)]
 		private float speed = 12f;
+
+		[SerializeField, Header("角色方向圖示")]
+		private Transform traDirection;
+
+		[SerializeField, Header("角色方向圖示範圍"), Range(0, 5)]
+		private float rangeDirection = 2.5f;
+		[SerializeField, Header("角色旋轉速度"), Range(0, 100)]
+		private float speedTurn = 1.5f;
+
+
 		private Rigidbody rig;
 		private void Awake()
 		{
@@ -24,6 +34,8 @@ namespace coolRain
 		private void Update()
 		{
 			//GetJoyStickValue();
+			UpdateDirectionIconPos();
+			LookDirectionIcon();
 		}
 
 		private void FixedUpdate()
@@ -46,6 +58,21 @@ namespace coolRain
 			//剛體.加速度 = 三維向量(X Y Z)
 			rig.velocity = new Vector3(joystick.Horizontal, 0, joystick.Vertical )* speed;
 
+		}
+
+		private void UpdateDirectionIconPos() 
+		{
+			Vector3 pos = transform.position + new Vector3(-joystick.Horizontal, 0.5f, -joystick.Vertical) * rangeDirection;
+			traDirection.position = pos;
+		} 
+		private void LookDirectionIcon() 
+		{
+			//取得面向角度 = 四位元.面向角度(方向圖示-角色位置)
+			Quaternion Look = Quaternion.LookRotation(traDirection.position - transform.position);
+			//角色角度設定 = 四位元.插值(角色角度 , 面向角度 , 轉速* 一幀的時間)
+			transform.rotation = Quaternion.Lerp(transform.rotation, Look, speedTurn * Time.deltaTime);
+			//角色的歐拉角 = 三維向量(0 ,原本的歐拉角.Y ,0)
+			transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
 		}
 	}
 }
